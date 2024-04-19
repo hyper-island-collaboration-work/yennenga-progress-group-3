356f5a700a6f040465d70ac063b15762e9308181
vsafonova <viktoriyablak@gmail.com>
2024-04-19 16:57:45 +0200

    Create method section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/heroSection.jsx/Hero";
+import OurMethods from "./components/methodsSection/OurMethods";
 
 function App() {
   return (
     <>
       <Hero />
+      <OurMethods/>
     </>
   );
 }
