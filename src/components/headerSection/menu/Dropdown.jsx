7033099092408ea1afe19d1b9ce0c3c78a931af6
vsafonova <viktoriyablak@gmail.com>
2024-04-19 17:00:29 +0200

    Work on hover efect for dropdown menu

--- src/components/headerSection/menu/Dropdown.jsx
+++ src/components/headerSection/menu/Dropdown.jsx
@@ -1,18 +1,36 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import DropdownMenuItem from "./DropdownMenuItem";
+import { useState } from "react";
 
 export default function Dropdown({ rootItem, items }) {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const handleMouseEnter = () => {
+    setDropdownVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownVisible(false);
+  };
+
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu
+      as="div"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="relative inline-block text-left"
+    >
       <div>
-        <Menu.Button className="inline-flex items-center justify-center gap-2">
+        <Menu.Button className=" inline-flex items-center justify-center gap-2">
           {rootItem}
-          <img src="/vector/dropdown.svg" alt="dropdown icon" />
+          <img src="/vector/dropdown.svg" alt="dropdown icon" 
+         />
         </Menu.Button>
       </div>
 
       <Transition
+        show={isDropdownVisible}
         as={Fragment}
         enter="transition ease-out duration-100"
         enterFrom="transform opacity-0 scale-95"
@@ -21,7 +39,9 @@ export default function Dropdown({ rootItem, items }) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute mt-[2.1rem] w-48 bg-black opacity-70">
+        <Menu.Items
+          className="absolute mt-[2.1rem] w-48 bg-black opacity-70"
+        >
           <div className="py-4">
             {items.map((item) => (
               <DropdownMenuItem>{item}</DropdownMenuItem>
