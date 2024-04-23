92c7e7c140d070e090454c167e6459f5105bcaea
MoysisPap <144357526+MoysisPap@users.noreply.github.com>
2024-04-23 17:02:30 +0200

    Footer_SocialMedia

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/headerSection/Header";
+import Footer from './components/footerSection/Footer';
+import SocialMedia from './components/footerSection/SocialMedia';
 
 
 function App() {
   return (
     <>
     <Header />
+    <Footer />
+    <SocialMedia />
     </>
   );
 }
