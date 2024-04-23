45197c2508e198899db467539674678f05e561cc
vsafonova <viktoriyablak@gmail.com>
2024-04-24 00:36:37 +0200

    add form styles and create custom radio buttons

--- src/components/Donation form/DonationForm.jsx
+++ src/components/Donation form/DonationForm.jsx
@@ -5,35 +5,50 @@ export default function DonationForm() {
     <form className="donation-form flex w-[440px] flex-col justify-between rounded-3xl bg-black bg-opacity-70 px-10 py-20 font-['Arial'] text-lg font-bold leading-10 text-light-gray">
       <div className="flex gap-2.5">
         <legend>Give as:</legend>
-        <label className="giveAs-btn">
+        <label className="custom-radioBtn-round">
           Private Person
           <input id="private" type="radio" value="private " name="private" />
           <span class="checkmark"></span>
         </label>
-        <label className="giveAs-btn">
+        <label className="custom-radioBtn-round">
           Company
           <input type="radio" id="company" name="company" value="Company" />
           <span class="checkmark"></span>
         </label>
       </div>
-      <div className="flex">
-        <button className="w-full rounded-bl-3xl rounded-tl-3xl border-2 border-light-gray shadow">
-          Donate
-        </button>
-        <button className="w-full rounded-br-3xl rounded-tr-3xl border-b-2 border-r-2 border-t-2 border-light-gray">
-          Give monthly
-        </button>
+      <div className="flex justify-center">
+        <label className="custom-radioBtn-big ">
+          <input id="donate" type="radio" value="donate" name="donate" />
+          <span class="checkmark rounded-bl-3xl rounded-tl-3xl border-2 border-light-gray px-11 py-2.5">
+            Donate
+          </span>
+        </label>
+        <label className="custom-radioBtn-big">
+          <input id="montly" type="radio" value="Give monthly" name="monthly" />
+          <span class="checkmark rounded-br-3xl rounded-tr-3xl border-b-2 border-r-2 border-t-2 border-light-gray px-11 py-2.5">
+            Give monthly
+          </span>
+        </label>
       </div>
-      <div className="flex">
-        <button className="w-full rounded-bl-3xl rounded-tl-3xl border-2 border-light-gray shadow">
-          200 kr
-        </button>
-        <button className="w-full border-b-2 border-t-2 border-light-gray">
-          400 kr
-        </button>
-        <button className="w-full rounded-br-3xl rounded-tr-3xl border-2 border-light-gray">
-          Optional
-        </button>
+      <div className="flex justify-center">
+        <label className="custom-radioBtn-big ">
+          <input id="200" type="radio" value="200" name="200" />
+          <span class="checkmark rounded-bl-3xl rounded-tl-3xl border-2 border-light-gray px-7 py-2.5">
+            200 kr
+          </span>
+        </label>
+        <label className="custom-radioBtn-big ">
+          <input id="400" type="radio" value="400" name="400" />
+          <span class="checkmark border-b-2 border-t-2 border-light-gray px-7 py-2.5">
+            400 kr
+          </span>
+        </label>
+        <label className="custom-radioBtn-big ">
+          <input id="400" type="radio" value="400" name="400" />
+          <span class="checkmark rounded-br-3xl rounded-tr-3xl border-2 border-light-gray px-7 py-2.5">
+            Optional
+          </span>
+        </label>
       </div>
       <div className="data-inputs text-light-gray++">
         <div>
