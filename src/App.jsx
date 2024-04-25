42f76f1618bcb6f0bd703cc7840929e91bf42fa6
simon-rosengren <simon.rosengren@outlook.com>
2024-04-25 11:24:31 +0200

    Added news section with connection to contentful.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/headerSection/Header";
+import RecentNews from "./components/newsSection/RecentNews";
 
 
 function App() {
   return (
     <>
     <Header />
+    <RecentNews />
     </>
   );
 }
