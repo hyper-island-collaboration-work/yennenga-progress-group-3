f88197083768daa9890840e364478e1c93f7623e
vsafonova <viktoriyablak@gmail.com>
2024-04-30 11:45:35 +0200

    udate vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
     // this points to the setup file created earlier
     setupFiles: ["./src/setupTests.js"],
   },
-
-})
+});
